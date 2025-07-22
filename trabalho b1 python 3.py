39eace19f00dbc63992e94afb2898bcf188cfe2a
wesley <wesleymentone17@gmail.com>
2025-07-22 20:41:40 -0300

    AcessoCliente

---
+++ trabalho b1 python 3.py
@@ -0,0 +1,7 @@
+username = input("Digite o nome de usuário: ")
+senha = input("Digite a senha: ")
+
+if username == "admin" and senha == "1234":
+    print("Acesso permitido")
+else:
+    print("Acesso negado")
